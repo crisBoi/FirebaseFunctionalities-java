36bf8cfb5c748d0c89977b85efd6db010a8d2e18
crisBoi <pranaycris@gmail.com>
2019-12-04 23:37:06 +0530

    authenticate working

---
+++ app/src/main/java/com/example/inhabitpototypr_0/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.inhabitpototypr_0;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    private List<AuthUI.IdpConfig> providers;
+
+    private Button signUpGoogleBtn;
+
+    public static final int RC_SIGN_IN = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        auth = FirebaseAuth.getInstance();
+        providers = new ArrayList<AuthUI.IdpConfig>();
+
+        providers.add(new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        signUpGoogleBtn = findViewById(R.id.login_btn);
+
+        signUpGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(), RC_SIGN_IN);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                Toast.makeText(this, "Welcome " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
+
